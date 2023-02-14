a180c8867b23dabb73651906fdaa3f6036c6dfe0
[Vanessa D'Antonio] <[yumin.dantonio@gmail.com]>
2023-02-14 08:35:51 +0000

    created card JS

--- assets/Javascript/openweather.js
+++ assets/Javascript/openweather.js
@@ -10,3 +10,34 @@ function createNavItem() {
   $("#navbarUl").append(navItem);
 
 }
+
+function createCard() {
+  // Create card elements
+  const weatherArea = $("#weather");
+
+  let containerDiv = $("<div>");
+  containerDiv.addClass("container-fluid");
+  weatherArea.append(containerDiv);
+
+  let rowDiv = $("<div>");
+  rowDiv.addClass("row");
+  rowDiv.attr("id", "weatherRow");
+  containerDiv.append(rowDiv);
+
+  let titleDiv = $("<div>");
+  titleDiv.addClass("col-12");
+  rowDiv.append(titleDiv);
+
+  let sectionTitle = $("<div>");
+  sectionTitle.addClass("section-title");
+  titleDiv.append(sectionTitle);
+
+  let weatherTitle = $("<h3>");
+  weatherTitle.append("Weather");
+
+  let lineDiv = $("<div>");
+  lineDiv.addClass("line");
+
+  sectionTitle.append(weatherTitle);
+  sectionTitle.append(lineDiv);
+}

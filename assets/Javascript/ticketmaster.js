a40a9425aeeb8f5a3dbde70502f5e05292f13f3d
whydidIchoosephysics <ionelvlad123@gmail.com>
2023-02-15 19:47:19 +0000

    added broken links and venue info

--- assets/Javascript/ticketmaster.js
+++ assets/Javascript/ticketmaster.js
@@ -74,6 +74,8 @@ function idShowInfo(eventID) {
 
       let imageLink = json.images[0].url;
 
+      let venueName = json._embedded.venues[0].name;
+
       let cardContainer = $("<div>");
       cardContainer.addClass("cardContainer col-lg-3 col-md-3 col-sm-12");
 
@@ -90,11 +92,15 @@ function idShowInfo(eventID) {
       let cardTitle = $("<h3>").text(eventName);
       cardTitle.addClass("card-title");
 
-      let cardText = $("<p>").text("Event 1");
+      let cardText = $("<p>").text(venueName);
       cardText.addClass("card-text");
 
-      let cardButton = $("<a>").text("See more");
+      let cardButton = $("<a>").text("Get tickets");
       cardButton.addClass("btn btn-primary");
+      cardButton.attr({
+        href: ticketLink,
+        target: "_blank",
+      });
 
       // Create card structure
       cardBody.append(cardImage, cardTitle, cardText, cardButton);
